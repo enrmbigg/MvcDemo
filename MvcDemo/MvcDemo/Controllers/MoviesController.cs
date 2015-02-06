4c4951e6e302b968c413fc69a85a750d22136065
Michael Biggs <mikeylfc007@blueyonder.co.uk>
2015-02-06 18:50:32 +0000

    Added Movie database and the model controller for the movie page

---
+++ MvcDemo/MvcDemo/Controllers/MoviesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDemo.Models;
+
+namespace MvcDemo.Controllers
+{ 
+    public class MoviesController : Controller
+    {
+        private MovieContext db = new MovieContext();
+
+        //
+        // GET: /Movies/
+
+        public ViewResult Index()
+        {
+            return View(db.Movies.ToList());
+        }
+
+        //
+        // GET: /Movies/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Movies movies = db.Movies.Find(id);
+            return View(movies);
+        }
+
+        //
+        // GET: /Movies/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Movies/Create
+
+        [HttpPost]
+        public ActionResult Create(Movies movies)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movies);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(movies);
+        }
+        
+        //
+        // GET: /Movies/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Movies movies = db.Movies.Find(id);
+            return View(movies);
+        }
+
+        //
+        // POST: /Movies/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Movies movies)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movies).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movies);
+        }
+
+        //
+        // GET: /Movies/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Movies movies = db.Movies.Find(id);
+            return View(movies);
+        }
+
+        //
+        // POST: /Movies/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Movies movies = db.Movies.Find(id);
+            db.Movies.Remove(movies);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcDemo/MvcDemo/Models/Movies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcDemo.Models
+{
+    public class Movies
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime Release { get; set; }
+    }
+
+    public class MovieContext : DbContext
+    {
+        public DbSet<Movies> Movies { get; set; } 
+    }
+}
